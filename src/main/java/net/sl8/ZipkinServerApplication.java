93932916f4963457eea22d7dc3dd38221fb0f53d
James Jurach <james.jurach@gmail.com>
2017-01-08 14:56:45 -0600

    enable initial service

--- src/main/java/net/sl8/ZipkinServerApplication.java
+++ src/main/java/net/sl8/ZipkinServerApplication.java
@@ -2,7 +2,9 @@ package net.sl8;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
 
+@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinServerApplication {
 
